d18d3be5319e41bd4041fde510d70b957b148ea9
Ray Bellis <ray@bellis.me.uk>
2020-08-13 11:12:37 +0100

    refactor h/w vecs into an inc file

--- lo.asm
+++ lo.asm
@@ -5892,17 +5892,4 @@ FOREVER 		BRA	FOREVER
 
 			ORG	$FFEA
 
-; Interrupt Vectors
-svec_SIO		FDB	hdlr_RST
-svec_CMI		FDB	hdlr_CMI
-svec_TRAP		FDB	hdlr_RST
-svec_DIV0		FDB	hdlr_DIV0
-svec_SWI3		FDB	hdlr_RST
-svec_SWI2		FDB	hdlr_SWI2
-svec_FIRQ		FDB	hdlr_RST
-svec_IRQ		FDB	hdlr_IRQ
-svec_SWI		FDB	hdlr_RST
-svec_NMI		FDB	hdlr_RST
-svec_RST		FDB	hdlr_RST
-
-			END
+			INCLUDE	"inc/vecs.asm"
