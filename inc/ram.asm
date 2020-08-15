609f46643582e94fd99af34b09b120fb7814524b
Ray Bellis <ray@bellis.me.uk>
2020-08-15 21:57:48 +0100

    yet more local labels

--- inc/ram.asm
+++ inc/ram.asm
@@ -4,8 +4,12 @@
 
 XROM			EQU	$0040	; storage for XROM call number
 
-USER_VOICE		EQU	$6001	; start of user voices (32 x 78 bytes = 0x9c0)
-USER_PFM		EQU	$6E45	; start of user PFMs (24 x 76 bytes = 0x720)
+USER_VOICE		EQU	$6001	; user voices (32 x 78 bytes = 2496)
+					; $6001 .. $69C0
+
+USER_PFM		EQU	$6E45	; user PFMs (24 x 76 bytes = 1824)
+					; $6E45 .. $7564
 
 LCD_COPY		EQU	$7D8B	; copy of current LCD output (32 bytes)
+
 LCD_BUFFER		EQU	$7DAB	; buffer for pending LCD output (32 bytes)
