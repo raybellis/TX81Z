0b35b0235d89ddc45c14fec19a2db2592f2320da
Ray Bellis <ray@bellis.me.uk>
2020-08-26 00:15:38 +0100

    add comments

--- inc/macros.asm
+++ inc/macros.asm
@@ -1,3 +1,6 @@
+;
+; Port 63 controls which ROM bank is selected
+;
 BANK_LO                 MACRO
 &0                      AIM     #%11110111,PORT6
                         ENDM
@@ -6,15 +9,17 @@ BANK_HI                 MACRO
 &0                      OIM     #%00001000,PORT6
                         ENDM
 
+;
+; Bank swap macro is conditional, depending on which bank we're in
+;
 			IFC	&BANK,HI
-
 BANK_SW                 MACRO
 &0                      AIM     #%11110111,PORT6
 			ENDM
-
 			ELSE
 BANK_SW                 MACRO
 &0                      OIM     #%00001000,PORT6
 			ENDM
-
 			ENDIF
+
+
