a9ecaa4bbbf906d28ac3659d852b14de34fad930
Ray Bellis <ray@bellis.me.uk>
2020-08-30 13:20:18 +0100

    more MIDI code annotated

--- inc/hw.asm
+++ inc/hw.asm
@@ -2,6 +2,9 @@
 ;* TX81Z Hardware memory map			    *
 ;****************************************************
 
+;
+; CPU registers
+;
 DDR2			EQU	$0001
 PORT2			EQU	$0003
 TCSR1			EQU	$0008
@@ -10,7 +13,7 @@ FRCL			EQU	$000A
 OCR1H			EQU	$000B
 TCSR2			EQU	$000F
 RMCR			EQU	$0010
-TRSCR			EQU	$0011
+TRCSR			EQU	$0011
 RDR			EQU	$0012
 TDR			EQU	$0013
 RP5CR			EQU	$0014
@@ -21,27 +24,36 @@ TCSR3			EQU	$001B
 TCONR			EQU	$001C
 T2CNT			EQU	$001D
 
+;
+; Yamaha YM2414 OPZ FM chip
+;
 OPZ_ADDRESS		EQU	$2000
 OPZ_DATA		EQU	$2001
 
+;
+; HD44780 16x02 LCD
+;
 LCD_CMD 		EQU	$4000
 LCD_DATA		EQU	$4001
 
+;
+; 8kB RAM ($8000 - $7FFF)
+;
 RAM			EQU	$6000
 
-;
-; TRCSR fields
-;
-RDRF			EQU	%10000000
-ORFE			EQU	%01000000
-TDRE			EQU	%00100000
-RIE			EQU	%00010000
-RE			EQU	%00001000
-TIE			EQU	%00000100
-TE			EQU	%00000010
-WU			EQU	%00000001
+;****************************************************
+; Field names for CPU IO registers		    *
+;****************************************************
 
-;
-; TCSR3 fields
-;
+; TRCSR
+RDRF			EQU	%10000000	; Receive Data Register Full
+ORFE			EQU	%01000000	; Overrun Framing Error
+TDRE			EQU	%00100000	; Transimit Data Register Empty
+RIE			EQU	%00010000	; Receive Interrupt Enable
+RE			EQU	%00001000	; Receive Enable
+TIE			EQU	%00000100	; Transmit Interrupt Enable
+TE			EQU	%00000010	; Transmit Enable
+WU			EQU	%00000001	; Wake-Up
+
+; TCSR3
 ECMI			EQU	%01000000	; Enable Counter Match Interrupt
--- inc/ram.asm
+++ inc/ram.asm
@@ -9,6 +9,7 @@ PTR_RX_TAIL		EQU	$00B6	; pointer for first MIDI RX
 PTR_TX_TAIL		EQU	$00B8	; pointer for last MIDI TX
 PTR_RX			EQU	$00BA	; pointer for next MIDI RX
 MIDI_TX_RUNSTAT		EQU	$00BC	; last MIDI command sent
+MIDI_CRC		EQU	$00C5	; rolling MIDI SysEx CRC
 MIDI_RX_ERR		EQU	$00CA	; non-zero if 
 
 USER_VOICE		EQU	$6001	; user voices (32 x 78 bytes = 2496)
@@ -20,7 +21,9 @@ USER_PFM		EQU	$6E45	; user PFMs (24 x 76 bytes = 1824)
 LCD_COPY		EQU	$7D8B	; copy of current LCD output (32 bytes)
 LCD_BUFFER		EQU	$7DAB	; buffer for pending LCD output (32 bytes)
 
-MIDI_TXBUF		EQU	$7DCC	; .. $7E0C (64 bytes)
+MIDI_TXBUF		EQU	$7DCC	; .. $7E0B (64 bytes)
+MIDI_TXBUF_END		EQU	$7E0B
+
 MIDI_RXBUF		EQU	$7E0C	; .. $7EE7 (220 bytes)
 MIDI_RXBUF_END		EQU	$7EE7	;
 
