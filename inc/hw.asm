988d57c6d8d9d07b82bf0dbc81e96f382e1dcf6e
Ray Bellis <ray@bellis.me.uk>
2020-08-31 18:04:36 +0100

    annotate more h/w port accesses

--- inc/hw.asm
+++ inc/hw.asm
@@ -45,52 +45,74 @@ RAM			EQU	$6000
 ; Field names for CPU IO registers		    *
 ;****************************************************
 
+; RP5CR ($xx)
+IRQ1E			EQU	%00000001	; IRQ1 Enable
+IRQ2E			EQU	%00000010	; IRQ2 Enable
+MRE			EQU	%00000100	; Memory Ready Enable
+RAME			EQU	%01000000	; RAM Enable
+
 ; TCSR1 ($08)
 EOCI1			EQU	%00001000	; Enable Output Compare Interrupt 1
 
+; TCSR2 ($0F)
+EOCI2			EQU	%00001000	; Enable Output Compare Interrupt 2
+
 ; TRCSR ($11)
-RDRF			EQU	%10000000	; Receive Data Register Full
-ORFE			EQU	%01000000	; Overrun Framing Error
-TDRE			EQU	%00100000	; Transimit Data Register Empty
-RIE			EQU	%00010000	; Receive Interrupt Enable
-RE			EQU	%00001000	; Receive Enable
-TIE			EQU	%00000100	; Transmit Interrupt Enable
-TE			EQU	%00000010	; Transmit Enable
 WU			EQU	%00000001	; Wake-Up
+TE			EQU	%00000010	; Transmit Enable
+TIE			EQU	%00000100	; Transmit Interrupt Enable
+RE			EQU	%00001000	; Receive Enable
+RIE			EQU	%00010000	; Receive Interrupt Enable
+TDRE			EQU	%00100000	; Transimit Data Register Empty
+ORFE			EQU	%01000000	; Overrun Framing Error
+RDRF			EQU	%10000000	; Receive Data Register Full
 
 ; TCSR3 ($1B)
+
+CKS00			EQU	%00000000	; Input Clock Select
+CKS01			EQU	%00000001
+CKS10			EQU	%00000010
+CKS11			EQU	%00000011
+
+TOS00			EQU	%00000000	; Timer Output Select
+TOS01			EQU	%00000100
+TOS10			EQU	%00001000
+TOS11			EQU	%00001100
+
+T2E			EQU	%00010000	; Timer 2 Enable
 ECMI			EQU	%01000000	; Enable Counter Match Interrupt
+CMF			EQU	%10000000	; Counter Match Flag
 
 ;****************************************************
 ; Field names for GPIO ports			    *
 ;****************************************************
 
 ; PORT6
-LED4			EQU	%10000000	; P67 - LED 4 (Utility)
-LED3			EQU	%01000000	; P66 - LED 3 (Edit / Compare)
-LED2			EQU	%00100000	; P65 - LED 2 (Play / Perform)
-LED1			EQU	%00010000	; P64 - LED 1 (Cursor)
-BANKSEL			EQU	%00001000	; P63 - ROM bank select
-SW10			EQU	%00000100	; P62 - SW10 (Utility)
-SW9			EQU	%00000010	; P61 - SW9 (Edit / Compare)
 SW8			EQU	%00000001	; P60 - SW8 (Play / Perform)
+SW9			EQU	%00000010	; P61 - SW9 (Edit / Compare)
+SW10			EQU	%00000100	; P62 - SW10 (Utility)
+BANKSEL			EQU	%00001000	; P63 - ROM bank select
+LED1			EQU	%00010000	; P64 - LED 1 (Cursor)
+LED2			EQU	%00100000	; P65 - LED 2 (Play / Perform)
+LED3			EQU	%01000000	; P66 - LED 3 (Edit / Compare)
+LED4			EQU	%10000000	; P67 - LED 4 (Utility)
 
 ; PORT5
-SW7			EQU	%10000000	; P57 - SW7 (PARAM-)
-SW6			EQU	%01000000	; P56 - SW6 (PARAM+)
-SW5			EQU	%00100000	; P55 - SW5 (DEC)
-SW4			EQU	%00010000	; P54 - SW4 (INC)
-						; P53 - unused
-						; P52 - MRDY
-SW11			EQU	%00000010	; P51 - SW11 (Store / EQ Copy)
 						; P50 - IRQ1
+SW11			EQU	%00000010	; P51 - SW11 (Store / EQ Copy)
+						; P52 - MRDY
+						; P53 - unused
+SW4			EQU	%00010000	; P54 - SW4 (INC)
+SW5			EQU	%00100000	; P55 - SW5 (DEC)
+SW6			EQU	%01000000	; P56 - SW6 (PARAM+)
+SW7			EQU	%10000000	; P57 - SW7 (PARAM-)
 
 ; PORT2
-SW3			EQU	%10000000	; P27 - SW3 (Master volume up)
-SW2			EQU	%01000000	; P26 - SW2 (Master volume down)
-SW1			EQU	%00100000	; P25 - SW1 (Cursor)
-						; P24 - MIDI Out
-						; P23 - MIDI In
-						; P22 - Clock input
-CASS_IN			EQU	%00000010	; P21 - Cassette interface input
 CASS_OUT		EQU	%00000001	; P20 - Cassette interface output
+CASS_IN			EQU	%00000010	; P21 - Cassette interface input
+						; P22 - Clock input
+						; P23 - MIDI In
+						; P24 - MIDI Out
+SW1			EQU	%00100000	; P25 - SW1 (Cursor)
+SW2			EQU	%01000000	; P26 - SW2 (Master volume down)
+SW3			EQU	%10000000	; P27 - SW3 (Master volume up)
